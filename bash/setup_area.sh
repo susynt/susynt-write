e366c39efd124fd36531bb90183c54d9f5ed9f83
gerbaudo <davide.gerbaudo@gmail.com>
2014-05-08 07:04:52 +0200

    import setup_area.sh from gerbaudo/susynt-prod-log

---
+++ bash/setup_area.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# Script to setup the area for submission of the SusyNt jobs.
+#
+# Based on Steve's instructions.
+# Requirements:
+# - access to the svn.cern.ch repositories
+# - have 'localSetuPandaClient' defined.
+#   This depends on your specifi setup; on gpatlas* these commands are defined with
+#   > export ATLAS_LOCAL_ROOT_BASE=/export/home/atlasadmin/ATLASLocalRootBase
+#   > source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+# - voms proxy
+#
+# Main steps:
+# - source this script with a tag, for example
+#   > source setup_area.sh
+# - this will create two directories :
+#   - 'prod_n0135' (with all the packages)
+#   - 'subm_n0135' (to submit the jobs)
+# - check that everything compiled (if not, fix it and 'create_tarball.sh'), then submit the jobs:
+#   > ./submit.py mc  -t n0135 --met Default -f <a-sample-list.txt> --nickname <my-nickname>
+#
+# davide.gerbaudo@gmail.com, Mar 2013
+
+PROD_DIR="prod"
+SUBM_DIR="subm"
+
+echo "Starting                          -- `date`"
+
+cd    ${PROD_DIR}
+
+svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/tags/SUSYTools-00-03-04               SUSYTools
+svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/tags/MultiLep-01-06-01    MultiLep
+
+sed -i -e '/asetup/s/^/#/' MultiLep/installscripts/install_script.sh # forget about asetup, we just need root
+localSetupROOT --rootVersion 5.34.18-x86_64-slc6-gcc4.7
+# the option below is the one needed for submit.py (see output of localSetupROOT)
+# --rootVer=5.34/18 --cmtConfig=x86_64-slc6-gcc47-opt
+
+source MultiLep/installscripts/install_script.sh
+
+echo "Done compiling                    -- `date`"
+
+cd ..
+
+# this part should be revised.  (DG 2014-05-07)
+
+# Is there any reason why the 'grid' stuff cannot go in a small
+# separate repo? In this way we can just always get the head (also,
+# this part does not contain code to be compiled, just submission
+# scripts and lists)
+
+# -- git clone git@github.com:gerbaudo/SusyCommon.git ${SUBM_DIR}
+# -- mv     ${SUBM_DIR}/SusyCommon/grid/* ${SUBM_DIR}/
+# -- rm -rf ${SUBM_DIR}/SusyCommon
+# -- cd    ${SUBM_DIR}
+# -- 
+# -- localSetupPandaClient
+# -- ./create_tarball.sh
+
+echo "Done, ready to submit jobs        -- `date`"
+
