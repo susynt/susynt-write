d3a127ab903301dc9d969a7450a391aebc20c6b4
gerbaudo <davide.gerbaudo@gmail.com>
2014-07-22 11:45:17 +0200

    update setup_area.sh for ntup_common
    This is the initial setup used to run over ntup common. Notes:
    1. Using a more recent SUSYTools than the latest stable one we used to
       NTUP_SUSY
    2. Using a home-brewed D3PDReader (missing branched with the
       'official' one)
    3. Because of 2. we now don't depend anymore on MultiLep (and on their
       installation script)

--- bash/setup_area.sh
+++ bash/setup_area.sh
@@ -2,45 +2,72 @@
 
 # Script to setup an area where we compile the packages needed to run NtMaker
 #
-# Based on Steve's instructions.
 # Requirements:
-# - access to the svn.cern.ch repositories
-# - have 'localSetuPandaClient' defined.
+# - access to the svn.cern.ch repositories (with kerberos auth)
+# - have 'localSetupROOT' defined.
 #   This depends on your specifi setup; on gpatlas* these commands are defined with
 #   > export ATLAS_LOCAL_ROOT_BASE=/export/home/atlasadmin/ATLASLocalRootBase
 #   > source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-# - voms proxy
 #
-# Main steps:
-# - source this script
-#   > source setup_area.sh
-# - this will create a directory `prod` with all the packages, and compile them
-# - try running NtMaker
-# davide.gerbaudo@gmail.com, Mar 2013
+# davide.gerbaudo@gmail.com, Jun 2014
 
-PROD_DIR="prod"
+function get_d3pdreader() {
+    # get the D3PDReader that I build
+    # (see https://twiki.cern.ch/twiki/bin/viewauth/AtlasProtected/D3PDMakerReader)
+    # The official one from atlasoff/PhysicsAnalysis/D3PDTools/D3PDReader has missing branches.
+    local FILENAME="D3PDReader_ntupcommon_v_0_1.tgz"
+    local ORIGIN=http://test-gerbaudo.web.cern.ch/test-gerbaudo/${FILENAME}
+    wget ${ORIGIN}
+    tar xzf ${FILENAME}
+    rm ${FILENAME}
+}
 
-echo "Starting                          -- `date`"
+function checkout_susytools_codebase() {
+    # checkout SUSYTools and all its dependencies
+    svn co -r 600009 svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/SUSYPhys/SUSYTools/trunk SUSYTools
+    SUSYTools/python/install.py
+}
 
-mkdir -p ${PROD_DIR}
-cd    ${PROD_DIR}
+function checkout_2lep_packages() {
+    # checkout the additional packages we need for the 2 lep
+    local SVN_WEAK_PROD="svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction"
+    svn co ${SVN_WEAK_PROD}/DGTriggerReweight/tags/DGTriggerReweight-00-00-29 DGTriggerReweight
+    svn co ${SVN_WEAK_PROD}/LeptonTruthTools/tags/LeptonTruthTools-00-01-07   LeptonTruthTools
+    svn co ${SVN_WEAK_PROD}/Mt2/tags/Mt2-00-00-01                             Mt2
+    svn co ${SVN_WEAK_PROD}/TriggerMatch/tags/TriggerMatch-00-00-10           TriggerMatch
+}
 
-git clone git@github.com:gerbaudo/SusyNtuple.git SusyNtuple
-cd SusyNtuple; git checkout SusyNtuple-00-01-11; cd -
-git clone git@github.com:gerbaudo/SusyCommon.git SusyCommon
-cd SusyCommon; git checkout SusyCommon-00-01-06; cd -
+function checkout_susynt_codebase() {
+    # checkout our own packages
+    git clone git@github.com:gerbaudo/SusyCommon.git SusyCommon
+    cd SusyCommon; git checkout -b ntup-common origin/ntup-common; cd -
+    git clone git@github.com:gerbaudo/SusyNtuple.git SusyNtuple
+    # for now using SusyNtuple master
+}
 
-svn co svn+ssh://svn.cern.ch/reps/atlasphys/Physics/SUSY/Analyses/WeakProduction/MultiLep/tags/MultiLep-01-06-08    MultiLep
+function compile_packages() {
+    localSetupROOT --rootVersion 5.34.18-x86_64-slc6-gcc4.7
+    : ${ROOTSYS:?"Need to set ROOTSYS to a valid location; missing ATLAS setup?"}
+    # the option below is the one needed for submit.py (see output of localSetupROOT)
+    # --rootVer=5.34/18 --cmtConfig=x86_64-slc6-gcc47-opt
+    source RootCore/scripts/setup.sh
+    rc find_packages
+    rc compile
+}
 
-sed -i -e '/asetup/s/^/#/' MultiLep/installscripts/install_script.sh # forget about asetup, we just need root
-localSetupROOT --rootVersion 5.34.18-x86_64-slc6-gcc4.7
-# the option below is the one needed for submit.py (see output of localSetupROOT)
-# --rootVer=5.34/18 --cmtConfig=x86_64-slc6-gcc47-opt
+#___________________________________________________________
+function main() {
 
-source MultiLep/installscripts/install_script.sh
+    local PROD_DIR="prod"
+    echo "Starting                          -- `date`"
+    mkdir -p ${PROD_DIR}
+    cd       ${PROD_DIR}
+    get_d3pdreader
+    checkout_susytools_codebase
+    checkout_2lep_packages
+    checkout_susynt_codebase
+    compile_packages
+    echo "Done compiling                    -- `date`"
+}
 
-echo "to fix SUSYTools-00-03-21 you need to" # tmp DG
-echo "sed -i  '/PACKAGE\_DEP/ s/$/ PhotonEfficiencyCorrection/' SUSYTools/cmt/Makefile.RootCore"
-
-echo "Done compiling                    -- `date`"
-
+main
