f12c4583b21e38f4eb012ee8565a0fd846278566
Daniel Joseph Antrim <dantrim@uci.edu>
2015-06-26 09:49:46 -0700

    Add "rcSetup -u" to bash/setup_release.sh

--- bash/setup_release.sh
+++ bash/setup_release.sh
@@ -9,7 +9,7 @@ export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
 setupATLAS
-rcSetup Base,2.3.14
+rcSetup -u; rcSetup Base,2.3.14
 rc find_packages
 rc compile
 
