be17306b539c6fb60e5220504f03093ef309d846
Daniel Joseph Antrim <dantrim@uclhc-1.ps.uci.edu>
2015-10-21 03:59:14 -0700

    update release chckout to new UI

--- bash/setup_release.sh
+++ bash/setup_release.sh
@@ -9,7 +9,8 @@ export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
 setupATLAS
-rcSetup -u; rcSetup Base,2.3.28
+lsetup "rcsetup -u"
+lsetup "rcsetup Base,2.3.28"
 rc find_packages
 rc clean
 rc compile
