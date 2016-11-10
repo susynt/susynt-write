c76e70ac11ced0ea80eb2d1161ac75e83fd9f59d
Daniel Antrim <dantrim@uci.edu>
2016-11-09 20:01:00 -0800

    add bad jet patch in setup

--- bash/setup_area.sh
+++ bash/setup_area.sh
@@ -143,7 +143,7 @@ function main {
     echo "Patching SUSYTools to include photon cleaning and ambiguity decorators"
     patch -p0 < patchAddPhotonDecorators.patch
     echo "Patching SUSYTools to not require OR to flag bad jets"
-    patch -p0 patchBadJetRemoveOR.patch
+    patch -p0 < patchBadJetRemoveOR.patch
 
     echo "Done                              -- `date`"
     echo "You can now go ahead and set-up the analysis release"
