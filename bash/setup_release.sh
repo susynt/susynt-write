2e80b5c7779ae6940816776d70bc7a5cecee3afe
Daniel Antrim <dantrim@uci.edu>
2017-06-28 00:39:08 -0700

    update setup_release

--- bash/setup_release.sh
+++ bash/setup_release.sh
@@ -8,7 +8,7 @@ echo "Setup up ATLAS soft"
 export ATLAS_LOCAL_ROOT_BASE="/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase"
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
-release="Base,2.4.24"
+release="Base,2.4.32"
 
 setupATLAS
 lsetup "rcsetup -u"
